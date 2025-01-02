4a960c261c49b87f2cd0a3cc0b711a032a5ffcb6
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2025-01-02 09:48:06 +0100

    wip buf
    Signed-off-by: Haile Lagi <52631736+hailelagi@users.noreply.github.com>

--- src/buffer_pool.rs
+++ src/buffer_pool.rs
@@ -1,1 +1,54 @@
 // Buffer pool.rs
+
+use io_uring::{IoUring, opcode};
+use std::sync::atomic::{AtomicU64, Ordering};
+use crossbeam_skiplist::SkipMap;
+use parking_lot::RwLock;
+use std::collections::VecDeque;
+
+pub struct BufferPool {
+    pages: SkipMap<u64, RwLock<Vec<u8>>>, 
+    meta: SkipMap<u64, RwLock<Page>>, 
+    
+    page_size: usize,
+    k_value: usize,
+    capacity: usize,
+
+    ring: IoUring,
+}
+
+impl BufferPool {
+    pub fn new(capacity: usize, page_size: usize, k_value: usize) -> std::io::Result<Self> {
+        Ok(BufferPool {
+            pages: SkipMap::new(),
+            meta: SkipMap::new(),
+            ring: IoUring::new(32)?,
+            page_size,
+            k_value,
+            capacity,
+        })
+    }
+    
+    pub async fn get_page(&self, page_id: u64) -> std::io::Result<&RwLock<Vec<u8>>> {
+        if let Some(page) = self.pages.get(&page_id) {
+            return Ok(page.value());
+        }
+
+        // todo: should this be atomic?
+        self.evict();
+        self.load_page(page_id).await
+    }
+    
+    fn evict(&self) {
+        if self.pages.len() >= self.capacity {
+            let victim = self.find_lru_k_victim();
+            if let Some(page_id) = victim {
+                self.evict_page(page_id);
+            }
+        }
+    }
+    
+    fn find_lru_k_victim(&self) -> Option<u64> {
+        todo!()
+    }
+}
